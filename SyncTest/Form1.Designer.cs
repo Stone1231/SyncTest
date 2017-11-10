7b5c185da7825816d84c33f55dd6e61d06ed6fea
Stone.F.Yang (netw devp.twtp01.Newegg) 61589 <sy43@buyabs.corp>
2017-11-10 11:06:16 +0800

    first

--- SyncTest/Form1.Designer.cs
+++ SyncTest/Form1.Designer.cs
@@ -28,12 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(243, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(29, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button0";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button0_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(29, 154);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Sleep";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.buttonSleep_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(29, 184);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "get Content";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.buttonContent1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(29, 214);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "get Content async";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.buttonContent2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- SyncTest/Form1.cs
+++ SyncTest/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +17,185 @@ namespace SyncTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            textBoxResults.Text = "Doing other thinds while waiting for that slow dude...\r\n";
+            textBoxResults.Text = ".......";
+            textBoxResults.Text += SlowDude();
+
+            (sender as Button).Enabled = true;
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            var slowTask = Task<string>.Factory.StartNew(() => SlowDude());
+            textBoxResults.Text = "Doing other thinds while waiting for that slow dude...\r\n";
+            var res = await slowTask;
+            textBoxResults.Text =".......";
+            textBoxResults.Text += res;
+
+            (sender as Button).Enabled = true;
+
+        }
+
+        private string SlowDude()
+        {
+            Thread.Sleep(4000);
+            return "Ta-dam! Here I am!\r\n";
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            textBoxResults.Text = "Doing other thinds while waiting for that slow dude...\r\n";
+            var res= SlowDude2();
+            textBoxResults.Text = ".......";
+            textBoxResults.Text += await res;
+
+            (sender as Button).Enabled = true;
+
+        }
+
+        async Task<string> SlowDude2()
+        {
+            return await Task<string>.Factory.StartNew(() =>
+            {
+                Thread.Sleep(4000);
+                return "Ta-dam! Here I am!\r\n";
+            });
+
+            //Task<string> task = Task<string>.Factory.StartNew(() =>
+            //{
+            //    Thread.Sleep(2000);
+            //    return "Ta-dam! Here I am!\r\n";
+            //});
+            //return await task;
+
+        }
+
+        private async void buttonSleep_Click(object sender, EventArgs e) {
+
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            Stopwatch sw = new Stopwatch();//引用stopwatch物件
+            sw.Reset();//碼表歸零
+            sw.Start();//碼表開始計時
+            /**************/
+            //await Sleep(1);
+            //await Sleep(1);
+            //await Sleep(1);
+
+            //比上面快
+            var s1 = Sleep(1);
+            var s2 = Sleep(1);
+            var s3 = Sleep(1);
+            await s1;
+            await s2;
+            await s3;
+            /**************/
+            sw.Stop();//碼錶停止
+            string result = sw.Elapsed.TotalMilliseconds.ToString();
+            textBoxResults.Text = result;
+
+            (sender as Button).Enabled = true;
+        }
+
+        async Task Sleep(int sec)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(sec * 1000);
+            });
+        }
+
+
+        private void buttonContent1_Click(object sender, EventArgs e) { 
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            var content = service.GetContent();
+            var count = service.GetCount();
+            var name = service.GetName();
+            watch.Stop();
+            textBoxResults.Text = watch.ElapsedMilliseconds.ToString();
+
+            (sender as Button).Enabled = true;
+        }
+
+        private async void buttonContent2_Click(object sender, EventArgs e)
+        {
+            (sender as Button).Enabled = false;
+            var textBoxResults = textBox1;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            ContentManagement service = new ContentManagement();
+            var contentTask = service.GetContentAsync();
+            var countTask = service.GetCountAsync();
+            var nameTask = service.GetNameAsync();
+
+            var content = await contentTask;
+            var count = await countTask;
+            var name = await nameTask;
+            watch.Stop();
+            textBoxResults.Text = watch.ElapsedMilliseconds.ToString();
+
+            (sender as Button).Enabled = true;
+        }
+    }
+
+
+
+    public class ContentManagement
+    {
+        public string GetContent()
+        {
+            Thread.Sleep(2000);
+            return "content";
+        }
+
+        public int GetCount()
+        {
+            Thread.Sleep(5000);
+            return 4;
+        }
+
+        public string GetName()
+        {
+            Thread.Sleep(3000);
+            return "Matthew";
+        }
+        public async Task<string> GetContentAsync()
+        {
+            await Task.Delay(2000);
+            return "content";
+        }
+
+        public async Task<int> GetCountAsync()
+        {
+            await Task.Delay(5000);
+            return 4;
+        }
+
+        public async Task<string> GetNameAsync()
+        {
+            await Task.Delay(3000);
+            return "Matthew";
         }
     }
 }
